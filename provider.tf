3943480dfcf22bfddb2ec45343b65a27e3a4d761
Conor Murphy <conmurph@cisco.com>
2021-10-21 14:29:28 +0200

    Adding output for kubeconfig

--- provider.tf
+++ provider.tf
@@ -56,8 +56,9 @@ data "intersight_kubernetes_cluster" "iks" {
 }
 
 provider "kubernetes" {
-    host = local.kube_config.clusters[0].cluster.server
-    client_certificate = base64decode(local.kube_config.users[0].user.client-certificate-data)
-    client_key = base64decode(local.kube_config.users[0].user.client-key-data)
-    cluster_ca_certificate = base64decode(local.kube_config.clusters[0].cluster.certificate-authority-data)
+  host                   = yamldecode(base64decode(data.intersight_kubernetes_cluster.iks.results[0].kube_config)).clusters[0].cluster.server
+  cluster_ca_certificate = base64decode(yamldecode(base64decode(data.intersight_kubernetes_cluster.iks.results[0].kube_config)).clusters[0].cluster.certificate-authority-data)
+  client_certificate     = base64decode(yamldecode(base64decode(data.intersight_kubernetes_cluster.iks.results[0].kube_config)).users[0].user.client-certificate-data)
+  client_key             = base64decode(yamldecode(base64decode(data.intersight_kubernetes_cluster.iks.results[0].kube_config)).users[0].user.client-key-data)
 }
+}  
