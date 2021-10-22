1f5904c60b8c2f64f30281d10410f0e6a50c7f05
conmurphy <conormurphy33@hotmail.com>
2021-10-22 22:23:57 +0200

    Setting up environment for new calendar service

--- applications.tf
+++ applications.tf
@@ -1,5 +1,5 @@
 variable "applications" {
   type = list(string)
-  default = ["healthcare-frontend","healthcare-predictor","healthcare-refund","healthcare-search"]
+  default = ["healthcare-frontend","healthcare-predictor","healthcare-refund","healthcare-search","healthcare-calendar"]
 
 }
