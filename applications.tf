e5f3fdd2b35c12e81e518041df31518b6e5c883a
conmurphy <conormurphy33@hotmail.com>
2021-10-24 09:14:21 +0200

    Update applications.tf

--- applications.tf
+++ applications.tf
@@ -1,6 +1,5 @@
 variable "applications" {
   type = list(string)
   #default = ["healthcare-frontend","healthcare-predictor","healthcare-refund","healthcare-search","healthcare-calendar"]
-  #default = ["healthcare-frontend"]
-  default = [""]
+  default = ["healthcare-frontend"]
 }
